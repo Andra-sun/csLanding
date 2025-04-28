f1bf0d95c69de80cbaf7d90b87030ae514308822
Andra-sun <camileguimaraes463@gmail.com>
2025-04-28 17:40:21 -0300

    footer atualizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 function Footer() {
     return (
-        <footer className="bg-black text-white p-4 text-center w-screen">
-            <p className="text-sm">Siga a FURIA nas redes sociais!</p>
-            <div>
+        <footer className="bg-black text-white p-6 text-center w-screen">
+            <p className="text-sm mb-2">Siga a FURIA nas redes sociais!</p>
+            <div className="mb-4">
                 <a
-                    href="https://www.instagram.com/furiagaming/"
+                    href="https://www.instagram.com/furiagg/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-neon mx-2"
+                    className="text-neon mx-2 hover:underline"
                 >
                     Instagram
                 </a>
@@ -15,11 +15,16 @@ function Footer() {
                     href="https://twitter.com/FURIA"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-neon mx-2"
+                    className="text-neon mx-2 hover:underline"
                 >
                     Twitter
                 </a>
             </div>
+            <p className="text-xs text-gray-400">
+                Este site foi desenvolvido para fins de demonstração. Algumas
+                informações exibidas podem ser fictícias ou meramente
+                ilustrativas.
+            </p>
         </footer>
     );
 }
