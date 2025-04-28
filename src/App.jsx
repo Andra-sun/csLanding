586766cd895bedf1e95bdcd2d3771dda67328d05
Andra-sun <camileguimaraes463@gmail.com>
2025-04-28 17:41:04 -0300

    footer adicionado a tela

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./components/landing/Landing";
 import Chat from "./components/chatFans/Chat";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { useState } from "react";
 import TeamIntro from "./components/landing/TeamIntro";
 
@@ -22,6 +23,7 @@ export default function App() {
                     <Routes>
                         <Route path="/" element={<Landing />} />
                     </Routes>
+                    <Footer />
                 </div>
 
                 {showChat && (
