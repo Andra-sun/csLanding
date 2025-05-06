d0942522fb8dfa8b6b30e4077e5cbc832dc1a80c
Andra-sun <camileguimaraes463@gmail.com>
2025-05-06 12:42:12 -0300

    exibição mobile ajustada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ function Header({ onJoinClick, isChatOpen }) {
     return (
         <header className="dark:bg-black bg-white shadow-md px-6 py-4 flex justify-between items-center w-full transition-all duration-300">
             <FuriaLogo className="dark:text-white text-black" />
-            <h2 className="font-bold text-xl">Nosso time de CS</h2>
+            <h2 className="font-bold text-xl hidden md:block">Nosso time de CS</h2>
             <button
                 onClick={() => {
                     navigate("/");
                     onJoinClick();
                 }}
-                className="dark:bg-white dark:hover:bg-gray-200 bg-black hover:bg-gray-800 dark:text-black text-white py-2 px-6 rounded-lg font-medium transition-all duration-300 cursor-pointer"
+                className="dark:bg-white dark:hover:bg-gray-200 bg-black hover:bg-gray-800 dark:text-black text-white py-2 px-6 rounded-lg font-medium transition-all duration-300 cursor-pointer text-nowrap"
             >
                 {isChatOpen ? "Fechar Chat" : "Juntar a torcida"}
             </button>
--- src/components/landing/TeamIntro.jsx
+++ src/components/landing/TeamIntro.jsx
@@ -3,7 +3,7 @@ import IconLogo from "../../assets/icon";
 function TeamIntro() {
     return (
         <section className="relative flex items-center justify-center h-screen dark:bg-black bg-white overflow-hidden">
-            <IconLogo className="absolute size-200 opacity-20" />
+            <IconLogo className="absolute md:size-200 size-170 opacity-20" />
 
             <div className="relative text-center dark:text-white text-gray-800">
                 <h1 className="text-4xl font-extrabold mb-4">
@@ -11,7 +11,6 @@ function TeamIntro() {
                 </h1>
                 <p className="text-lg dark:text-gray-300 max-w-xl mx-auto">
                     O time brasileiro que desafia o mundo no Counter-Strike.
-                    Aqui você acompanha tudo!
                 </p>
             </div>
         </section>
